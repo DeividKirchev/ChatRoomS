7e891679d2c0297a9480d8446f73737b5e3d7f77
Deivid Kirchev <davidkirchevbg@gmail.com>
2022-07-06 16:30:05 +0300

    adding an updating chat

--- Form1.cs
+++ Form1.cs
@@ -9,33 +9,36 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using System.Net;
+using System.Threading;
 namespace ChatRoomS
 {
     public partial class Form1 : Form
     {
         private int user_id;
         private int cr_id;
+        private int selected_index = -1;
         public string connString = "server=192.168.0.109;port=3306;user id=root;password=asdasdasd123AS;persistsecurityinfo=True;database=chatroomdb";
         MySqlConnection conn;
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
             try
             {
                 conn = new MySqlConnection(connString);
                 conn.Open();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
             }
             string hostName = Dns.GetHostName();
             string IP = Dns.GetHostByName(hostName).AddressList[0].ToString();
-            MySqlCommand cmd = new MySqlCommand("SELECT * FROM USERS WHERE IP LIKE '"+IP+"'",conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM USERS WHERE IP LIKE '" + IP + "'", conn);
             MySqlDataReader dataReader = cmd.ExecuteReader();
-            if(dataReader.Read())
+            if (dataReader.Read())
             {
-                label_id.Text = "ID - #"+dataReader["ID"].ToString();
+                label_id.Text = "ID - #" + dataReader["ID"].ToString();
                 user_id = Int32.Parse(dataReader["ID"].ToString());
             }
             else
@@ -43,28 +46,38 @@ namespace ChatRoomS
                 dataReader.Close();
                 cmd.CommandText = "INSERT INTO USERS(NAME,PASSWORD,EMAIL,IP) VALUES('test','test','test','" + IP + "')";
                 cmd.ExecuteNonQuery();
-            
-                cmd = new MySqlCommand("SELECT * FROM USERS WHERE IP LIKE '"+IP+"'", conn);
+
+                cmd = new MySqlCommand("SELECT * FROM USERS WHERE IP LIKE '" + IP + "'", conn);
                 dataReader = cmd.ExecuteReader();
-                label_id.Text = "ID - #"+dataReader["ID"].ToString();
+                label_id.Text = "ID - #" + dataReader["ID"].ToString();
                 user_id = Int32.Parse(dataReader["ID"].ToString());
             }
             dataReader.Close();
-            cmd = new MySqlCommand("SELECT * FROM Chatroom WHERE user1_id = " + user_id + " or user2_id = " + user_id, conn);
-            dataReader = cmd.ExecuteReader();
-            while(dataReader.Read())
+            getChatrooms();
+            Thread background = new Thread(checker);
+            background.IsBackground = true;
+            background.Start();
+        }
+        public void getChatrooms()
+        {
+            ChatRooms.Items.Clear();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Chatroom WHERE user1_id = " + user_id + " or user2_id = " + user_id, conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
             {
                 ChatRooms.Items.Add("#" + dataReader["user1_id"] + " and #" + dataReader["user2_id"]);
             }
             dataReader.Close();
+            if (selected_index != -1) ChatRooms.SelectedIndex = selected_index;
         }
-        public static int NewRoomID ;
+        public static int NewRoomID;
         private void Add_Click(object sender, EventArgs e)
         {
             NewRoomID = 0;
             AddChatroom addChatroom = new AddChatroom();
             addChatroom.ShowDialog();
-            if(NewRoomID!=0)
+            if (NewRoomID != 0)
             {
                 try
                 {
@@ -86,6 +99,8 @@ namespace ChatRoomS
         }
         private void setChat(int index)
         {
+            //Chat.AppendText("1");
+            Chat.Clear();
             MySqlCommand cmd = new MySqlCommand("SELECT * FROM CHATROOM WHERE USER1_ID = " + index + " AND USER2_ID =" + user_id + " OR USER2_ID = " + index + " AND USER1_ID =" + user_id, conn);
             MySqlDataReader dataReader = cmd.ExecuteReader();
             if (!dataReader.Read())
@@ -94,17 +109,17 @@ namespace ChatRoomS
                 return;
             }
             int croom_id = Int32.Parse(dataReader["id"].ToString());
-            if (cr_id == croom_id)
+            /*if (cr_id == croom_id)
             {
                 dataReader.Close();
                 return;
-            }
+            }*/
             cr_id = croom_id;
             //Chat.Text = chatroom_id.ToString();
             dataReader.Close();
             cmd = new MySqlCommand("SELECT * FROM MESSAGES WHERE CR_ID = " + cr_id + " ORDER BY DATE ASC", conn);
             dataReader = cmd.ExecuteReader();
-            while(dataReader.Read())
+            while (dataReader.Read())
             {
                 Chat.AppendText("USER " + dataReader["user_id"].ToString() + ": " + dataReader["message"].ToString() + Environment.NewLine);
             }
@@ -113,19 +128,21 @@ namespace ChatRoomS
         private int getUserID(string s)
         {
             int pos = s.IndexOf('#');
-            string s1 = s.Substring(pos+1, 1);
+            string s1 = s.Substring(pos + 1, 1);
             int num1 = Int32.Parse(s1);
             if (num1 != user_id) return num1;
 
             s.Remove(pos, 1);
             pos = s.IndexOf('#');
-            s1 = s.Substring(pos+1, 1);
+            s1 = s.Substring(pos + 1, 1);
             num1 = Int32.Parse(s1);
             return num1;
 
         }
         private void ChatRooms_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (ChatRooms.SelectedItem == null) return;
+            selected_index = ChatRooms.SelectedIndex;
             int index;
             index = getUserID(ChatRooms.SelectedItem.ToString());
             //Chat.Text = index.ToString();
@@ -135,11 +152,27 @@ namespace ChatRoomS
         private void btnSend_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == null) return;
-            Chat.AppendText("USER " + user_id +": " + textBox1.Text + Environment.NewLine);
-             //Chat.AppendText(DateTime.Now.ToString());
-              MySqlCommand cmd = new MySqlCommand("INSERT INTO MESSAGES(CR_ID,USER_ID,MESSAGE,DATE) VALUES(" + cr_id + "," + user_id + ",'" + textBox1.Text + "', str_to_date('" + DateTime.Now.ToShortDateString()+ "','%d/%m/%Y %T'))", conn);
-             cmd.ExecuteNonQuery();
-              textBox1.Clear();
+            Chat.AppendText("USER " + user_id + ": " + textBox1.Text + Environment.NewLine);
+            //Chat.AppendText(DateTime.Now.ToString());
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO MESSAGES(CR_ID,USER_ID,MESSAGE,DATE) VALUES(" + cr_id + "," + user_id + ",'" + textBox1.Text + "', str_to_date('" + DateTime.Now.ToShortDateString() + "','%d/%m/%Y %T'))", conn);
+            cmd.ExecuteNonQuery();
+            textBox1.Clear();
+        }
+
+        void checker()
+        {
+            while (true)
+            {
+                getChatrooms();
+                if (selected_index != -1) ChatRooms.SelectedIndex = selected_index;
+                /* if (ChatRooms.SelectedItem != null)
+                 {
+                     int index;
+                     index = getUserID(ChatRooms.SelectedItem.ToString());
+                     setChat(index);
+                 }*/
+                Thread.Sleep(2000);
+            }
         }
     }
 }
